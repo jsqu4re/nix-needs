582ee5a9345d0f16114b6381d57961b3c0da7608
Johannes Jeising <jeising@magazino.eu>
2024-07-15 14:46:39 +0200

    add lacy sphinxcontrib plantuml

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Sphinx Needs overlay for Nixpkgs";
 
   outputs = { self, ... }: {
-    overlays.sphinx-needs = import ./sphinx-needs-overlay.nix;
+    overlays.sphinx-needs = import ./overlays/sphinx-needs.nix;
+    overlays.sphinx-needs-lazy = import ./overlays/sphinx-needs-lazy.nix;
 
     overlays.default = self.overlays.sphinx-needs;
   };
---
+++ overlays/sphinx-needs-lazy.nix
@@ -0,0 +1,25 @@
+self: super:
+
+{
+  pythonPackagesExtensions = super.pythonPackagesExtensions ++ [
+    (
+      python-self: python-super: {
+        sphinxcontrib-plantuml = python-super.sphinxcontrib-plantuml.overrideAttrs (old: {
+          patches = (old.patches or []) ++ [
+            ../pkgs/patches/lazyloading.patch
+          ];
+        });
+        sphinx-needs = python-self.callPackage ../pkgs/sphinx-needs.nix{
+          sphinx-data-viewer = python-self.sphinx-data-viewer;
+        };
+        sphinxcontrib-test-reports = python-self.callPackage ../pkgs/sphinxcontrib-test-reports.nix{
+          sphinx-needs = python-self.sphinx-needs;
+        };
+        sphinx-data-viewer = python-self.callPackage ../pkgs/sphinx-data-viewer.nix{ };
+        sphinx-simplepdf = python-self.callPackage ../pkgs/sphinx-simplepdf.nix{ };
+        sphinx-preview = python-self.callPackage ../pkgs/sphinx-preview.nix{ };
+        sphinx-immaterial = python-self.callPackage ../pkgs/sphinx-immaterial.nix{ };
+      }
+    )
+  ];
+}
